f11f17ad56495ddcbd94de7315d4476d7f587816
LeandroSilva <leandro.silva@virtual-identity.com>
2024-08-09 15:19:11 +0200

    fix: CARITAS-243 * remove UPLOAD_FILE_TO_FEEDBACK_ROOM_METHOD_NAME

--- src/test/java/de/caritas/cob/uploadservice/helper/MethodAndParameterConstants.java
+++ src/test/java/de/caritas/cob/uploadservice/helper/MethodAndParameterConstants.java
@@ -19,7 +19,6 @@ public class MethodAndParameterConstants {
         String.class,
         String.class
       };
-  public static final String UPLOAD_FILE_TO_FEEDBACK_ROOM_METHOD_NAME = "uploadFileToFeedbackRoom";
   public static final Class<?>[] UPLOAD_FILE_TO_FEEDBACK_ROOM_METHOD_PARAMS =
       new Class[] {
         String.class,
